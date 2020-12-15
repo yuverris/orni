073fbece54faf2a067bd4ad2ae3b4d512c8e4dfb
Dammirbuh <walidakka53@gmail.com>
2020-12-15 15:15:30 +0100

    added examples, fixed some trashcode

---
+++ examples/rest_api.cpp
@@ -0,0 +1,33 @@
+#include "../src/orni_http_server.hpp"
+#include <nlohmann/json.hpp> //  or use any json library
+
+int main(){
+    orni::HttpServer app;
+    nlohmann::json jsonRes = {
+        {"posts", nlohmann::json::array()}
+    };
+    app.route("/posts", [&](orni::Request&& rew, orni::Response&& res){
+            res.set("Content-Type", "application/json");
+            res.setStatus(200);
+            res.send(jsonRes.dump(4));
+            res.dump();
+    });
+    app.route("/new", [&](orni::Request&& req, orni::Response&& res){
+            if (req.Method == "POST") {
+                auto data = nlohmann::json::parse(req.Body);
+                jsonRes["posts"].push_back(data);
+                res.setStatus(201);
+                res.send("created");
+                res.dump();
+            } else if (req.Method == "GET") {
+                nlohmann::json example;
+                example["id"] = nullptr;
+                example["author"] = nullptr;
+                example["title"] = nullptr;
+                res.set("Content-Type", "application/json");
+                res.send(example.dump(4));
+                res.dump();
+            }
+    });
+    app.run(5000);
+}
