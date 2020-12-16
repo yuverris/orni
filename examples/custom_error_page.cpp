4e41d42d299d6c3b067cda4b7684505134173887
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-12-16 11:56:09 +0100

    Update custom_error_page.cpp

--- examples/custom_error_page.cpp
+++ examples/custom_error_page.cpp
@@ -9,6 +9,6 @@ int main() {
     app.setServerError([](orni::Request&& req, orni::Response&& res) {
             res.send("my custom 500 page");
             res.dump();
-    });  //  for habdling 500 swrver errors
+    });  //  for habdling 500 server errors
     app.run(1234); //  or any port default to 5000
 }
