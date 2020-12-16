4429c614e09bc773f7480f12e8f02dadc643f364
Dammirbuh <walidakka53@gmail.com>
2020-12-16 10:58:15 +0100

    added res.redirect, res.Queries, logger

--- src/httpparser/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "urlparser.h"
-#include <iostream>
-
-int main() {
-    httpparser::UrlParser pars;
-    const char url[] = "https://example.com/lmao/bruh?id=5";
-    pars.parse(url);
-    std::cout << pars.path() << " " << pars.query() << '\n';
-}
